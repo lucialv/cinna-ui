7630cfafee848aa61fccf8982e0fcff7657a0d3b
lucialv <lucia.alvrzt@gmail.com>
2024-01-13 20:59:44 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,66 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "monokai",
+    },
+  },
+  integrations: [
+    starlight({
+      title: "Cinna UI",
+      logo: {
+        src: "./src/assets/sakura.png",
+        replacesTitle: false,
+      },
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            href: "./src/assets/sakura.png",
+            rel: "icon",
+            type: "image/png",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "theme-color",
+            content: "#F600E0",
+          },
+        },
+      ],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Getting Started",
+              link: "/getting-started/introduction/",
+            },
+          ],
+        },
+        {
+          label: "Components",
+          autogenerate: {
+            directory: "components",
+          },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+});
---
+++ src/content/docs/getting-started/introduction.md
@@ -0,0 +1,10 @@
+---
+title: Itroduction
+description: Introduction to the Cinna UI website.
+---
+
+Cinna UI is a component library for Tailwind CSS, you just need to copy the code and paste it into your project, and you will have a beautiful website.
+
+## Why Cinna UI?
+
+I created Cinna UI because I wanted to have a component library for Tailwind CSS, and I didn't find any that I liked, so I decided to create my own. I hope you like it.
